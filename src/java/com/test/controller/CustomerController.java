a9f88b4e275f1993a50c76b6beac5e7de0e10304
peondusud <reflexion_31@hotmail.com>
2012-10-14 01:53:20 +0200

    remove unused files

--- src/java/com/test/controller/CustomerController.java
+++ src/java/com/test/controller/CustomerController.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.test.controller;
 
 /**
@@ -11,12 +7,10 @@ package com.test.controller;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.peon.core.*;
-import org.peondusud.contact.ContactManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,67 +19,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class CustomerController {
 
-    org.peondusud.contact.ContactManager contactMan = new org.peondusud.contact.ContactManager(org.peondusud.contact.ContactManager.PopulateContact());
-
     protected ModelAndView handleRequestInternal(HttpServletRequest hsr, HttpServletResponse hsr1) throws Exception {
         ModelAndView model = null;
 
         return model;
     }
 
-//    @RequestMapping(value = "/show", method = RequestMethod.GET)
-//    public ModelAndView get(HttpServletRequest hsr, HttpServletResponse hsr1) {
-//        HttpSession session = hsr.getSession();
-//        String userLoginCookie = (String) session.getAttribute("username");
-//        String userPassCookie = (String) session.getAttribute("password");
-//
-//        if (userPassCookie == null || userLoginCookie == null) {
-//            String str = "not logged";
-//            return new ModelAndView("error", "str", str);
-//        }
-//
-//        User usrLogin = Appz.getInstance().userPresentLogin(userLoginCookie);
-//        //usrLogin.getPassword() != userPassCookie.toString()
-//        if (!usrLogin.getPassword().equals(userPassCookie.toString())) {
-//
-//            String str = "login & password no match same user";
-//            return new ModelAndView("error", "str", str);
-//        }
-//
-//        ArrayList<Contact> arrContact = usrLogin.getUserData().getTableContact();
-//        org.peondusud.contact.ContactManager arrContactMan = new ContactManager();
-//        ArrayList<org.peondusud.contact.Contact> arrCont = new ArrayList<org.peondusud.contact.Contact>();
-//        Iterator<Contact> itr = arrContact.iterator();
-//        while (itr.hasNext()) {
-//            org.peondusud.contact.Contact tmpContact = new org.peondusud.contact.Contact();
-//            Contact element = itr.next();
-//            tmpContact.setNom(element.getName());
-//            tmpContact.setPrenom(element.getSurname());
-//            tmpContact.setBrithday(element.getBrithday());
-//
-//            ArrayList<String> arrStr = new ArrayList<String>();
-//            arrStr.add(element.getPhones());
-//            tmpContact.setPhones(arrStr);
-//
-//            ArrayList<String> arrStr2 = new ArrayList<String>();
-//            arrStr2.add(element.getEmails());
-//            tmpContact.setEmails(arrStr2);
-//
-//            ArrayList<org.peondusud.contact.Address> tmpArrayAddr = new ArrayList<org.peondusud.contact.Address>();
-//            Iterator<Address> itrAddr = usrLogin.getUserData().getAddressAssociatedToContact(element).iterator();
-//            while (itrAddr.hasNext()) {
-//                Address elementAddr = itrAddr.next();
-//                tmpArrayAddr.add(new org.peondusud.contact.Address(elementAddr.getNickAddress(), elementAddr.getNumber(), elementAddr.getRue(), elementAddr.getCp(), elementAddr.getVille(), elementAddr.getPays()));
-//
-//            }
-//            tmpContact.setAddrs(tmpArrayAddr);
-//            arrCont.add(tmpContact);
-//            //arrContactMan.arrContact.add(tmpContact);
-//        }
-//        arrContactMan.setArrContact(arrCont);
-//        //return new ModelAndView("show_list_2", "arrContact", arrContact);
-//        return new ModelAndView("show_list_1", "arrContactMan", arrContactMan);
-//    }
     @RequestMapping(value = "/list_add", method = RequestMethod.GET)
     public ModelAndView list(HttpServletRequest hsr, HttpServletResponse hsr1) throws Exception {
         HttpSession session = hsr.getSession();
@@ -171,41 +110,6 @@ public class CustomerController {
         }
     }
 
-//
-//    @RequestMapping(value = "/addd", method = RequestMethod.POST)
-//    public ModelAndView saved(HttpServletRequest hsr, HttpServletResponse hsr1) throws Exception {
-//        //TODO teste si champ vide
-//        Object prenom = hsr.getParameter("prenom");
-//        Object nom = hsr.getParameter("nom");
-//        Object phone = hsr.getParameter("phone");
-//        Object mail = hsr.getParameter("mail");
-//        Object addr_nick = hsr.getParameter("addr_nick");
-//        Object addr_nb = hsr.getParameter("addr_nb");
-//        Object addr_rue = hsr.getParameter("addr_rue");
-//        Object addr_cp = hsr.getParameter("addr_cp");
-//        Object addr_ville = hsr.getParameter("addr_ville");
-//        Object addr_pays = hsr.getParameter("addr_pays");
-//        Object birthday = hsr.getParameter("birthday");
-//        HttpSession session = hsr.getSession();
-//        String userLoginCookie = (String) session.getAttribute("username");
-//        String userPassCookie = (String) session.getAttribute("password");
-//
-//        if (userPassCookie == null || userLoginCookie == null) {
-//            String str = new String("not logged");
-//            return new ModelAndView("error", "str", str);
-//        }
-//
-//        User usrLogin = Appz.getInstance().userPresentLogin(userLoginCookie);
-//        ArrayList<Contact> arrContact = usrLogin.getUserData().getTableContact();
-//
-//
-//        Address tmpAddr = new Address(addr_nb.toString(), addr_rue.toString(), addr_ville.toString(), addr_cp.toString(), addr_pays.toString());
-//        Contact tmpCont = new Contact(nom.toString(), prenom.toString(), mail.toString(), phone.toString(), birthday.toString());
-//        // Contact tmp = Appz.getInstance().getDataBase().get( Appz.getInstance().indexPresentLogin(userLoginCookie) ).getUserData().getTableContact().get(index);
-//        Appz.getInstance().addContact(userLoginCookie, tmpCont, tmpAddr);
-//        ArrayList<Contact> arrContactMan = adpaterPeonCore(usrLogin);
-//        return new ModelAndView("show_list_1", "arrContactMan", arrContactMan);
-//    }
     @RequestMapping(value = "/modify_contact", method = RequestMethod.GET)
     public ModelAndView modify_contact_page(HttpServletRequest hsr, HttpServletResponse hsr1) throws Exception {
         //indice  modID arraylist pour choix contact
@@ -280,8 +184,7 @@ public class CustomerController {
 
             ArrayList<Contact> arrContact = Appz.getInstance().getArrContact(username);
             return new ModelAndView("list_show", "arrContact", arrContact);
-        }
-         else {
+        } else {
             String str = "please Log in ";
             return new ModelAndView("error", "str", str);
         }
@@ -293,11 +196,6 @@ public class CustomerController {
         return new ModelAndView("index");
     }
 
-//    @RequestMapping(value = "/list", method = RequestMethod.GET)
-//    public ModelAndView list() {
-//
-//        return new ModelAndView("show_list", "contactMan", contactMan);
-//    }
     @RequestMapping(value = "/delete", method = RequestMethod.GET)
     public ModelAndView delete(HttpServletRequest hsr, HttpServletResponse hsr1) throws Exception {
 
@@ -394,41 +292,5 @@ public class CustomerController {
             return new ModelAndView("redirect:/");
         }
 
-    }
-
-    private ArrayList<Contact> adpaterPeonCore(User usrLogin) {
-        ArrayList<Contact> arrContact = usrLogin.getUserData().getTableContact();
-        org.peondusud.contact.ContactManager arrContactMan = new ContactManager();
-        ArrayList<org.peondusud.contact.Contact> arrCont = new ArrayList<org.peondusud.contact.Contact>();
-        Iterator<Contact> itr = arrContact.iterator();
-        while (itr.hasNext()) {
-            org.peondusud.contact.Contact tmpContact = new org.peondusud.contact.Contact();
-            Contact element = itr.next();
-            tmpContact.setNom(element.getName());
-            tmpContact.setPrenom(element.getSurname());
-            tmpContact.setBrithday(element.getBrithday());
-
-            ArrayList<String> arrStr = new ArrayList<String>();
-            arrStr.add(element.getPhones());
-            tmpContact.setPhones(arrStr);
-
-            ArrayList<String> arrStr2 = new ArrayList<String>();
-            arrStr2.add(element.getEmails());
-            tmpContact.setEmails(arrStr2);
-
-            ArrayList<org.peondusud.contact.Address> tmpArrayAddr = new ArrayList<org.peondusud.contact.Address>();
-            Iterator<Address> itrAddr = usrLogin.getUserData().getAddressAssociatedToContact(element).iterator();
-            while (itrAddr.hasNext()) {
-                Address elementAddr = itrAddr.next();
-                tmpArrayAddr.add(new org.peondusud.contact.Address(elementAddr.getNickAddress(), elementAddr.getNumber(), elementAddr.getRue(), elementAddr.getCp(), elementAddr.getVille(), elementAddr.getPays()));
-
-            }
-            tmpContact.setAddrs(tmpArrayAddr);
-            arrCont.add(tmpContact);
-            //arrContactMan.arrContact.add(tmpContact);
-        }
-        arrContactMan.setArrContact(arrCont);
-
-        return arrContact;
     }
 }
--- web/WEB-INF/jsp/index.jsp
+++ web/WEB-INF/jsp/index.jsp
@@ -9,18 +9,9 @@
     </head>
 
     <body>
-        <p>Hello! This is the default welcome page for a Spring Web MVC project.</p>
-        <p><i>To display a different welcome page for this project, modify</i>
-            <tt>index.jsp</tt> <i>, or create your own welcome page then change
-                the redirection in</i> <tt>redirect.jsp</tt> <i>to point to the new
-                welcome page and also update the welcome-file setting in</i>
-            <tt>web.xml</tt>.</p>
-        <A HREF="login.html">login</a></br>
-        <A HREF="signin.html">signin</a></br>
-        <A HREF="list.html">LIST</a></br>
-        <A HREF="add.html">Add contact</a></br>
-        </br>
-        </br>
+        <p>Hello!</p>
+        <p><i> This is the default welcome page for a Spring Web MVC project.</i></p>
+
         </br>
         <div class=header>	
             <jsp:include page="header.jsp"/> 
